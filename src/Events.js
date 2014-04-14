1e03f703a2ed3411768b0d797a70071bc2a26ba1
Parth Shah <parthshahva@gmail.com>
2014-04-14 10:10:09 -0500

    all tests passing

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,14 @@
 var Events = {
-
+  on: function(item, func){
+    if(this.events === undefined)
+      {this.events = {}};
+    if(this.events[item]===undefined)
+    {this.events[item] = []};
+    this.events[item].push(func);
+  },
+  trigger: function(event){
+    for (i=0; i<this.events[event].length; i+=1) {
+        this.events[event][i]();
+    }
+  }
 };
